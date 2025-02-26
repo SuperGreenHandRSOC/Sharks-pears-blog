0556581277ac50d0ca87ed970efb8297b51e6e5e
SGH_RSOC <imantares@yeah.net>
2025-02-26 19:01:20 +0800

    modify profile

--- src/app/_ui/infos.ts
+++ src/app/_ui/infos.ts
@@ -1,5 +1,5 @@
 export const name = "Sharks' Pear"
-export const intro = "Welcome to my homepage. Currently an Undergraduate majored in AI. Have been interested in above-mentioned topics recently. Welcome to contact me and catch a chat. ^v^"
+export const intro = "Welcome to my homepage. Currently an Undergraduate majored in AI. Have been interested in above-mentioned topics recently. Welcome to contact me if you have something to share. ^v^"
 export const platform = [
     {name:"github", href:"https://github.com/SuperGreenHandRSOC", src:"github.svg"},
     {name:"X", href:"https://x.com/AlienTurning", src:"x.svg"}
